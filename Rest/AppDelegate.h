3bd447b6528759dd64e5fb72c46f63ce07d19f9a
Alexander Massuda <alex@macbook-pro-de-alexander.local>
2017-05-30 23:54:21 -0300

    Initial Commit

---
+++ Rest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rest
+//
+//  Created by Alexander Massuda on 30/05/17.
+//  Copyright © 2017 alexcaiom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rest
+//
+//  Created by Alexander Massuda on 30/05/17.
+//  Copyright © 2017 alexcaiom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
